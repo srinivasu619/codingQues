b3858dc3aef818ba0e51330a62d31f9f04599805
srinivasu619 <srinivasu.618@gmail.com>
2017-11-06 23:10:26 +0530

    Added Solution for the No of Alpha Codes(initial)

---
+++ Solution32.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Solution32 {
+
+	public static int give_no(String number)
+    {
+      int output [] = new int[number.length()+1];
+      output[0] = 1;
+      output[1] = 1;
+      for(int i=2;i<=number.length();i++)
+      {
+        output[i] = output[i-1]%1000000007;
+        int num = (number.charAt(i-2)-'0')*10+(number.charAt(i-1)-'0');
+        if(num <= 26)
+        {
+          output[i] = (output[i]%1000000007+output[i-2]%1000000007)%1000000007;
+        }
+      }
+      return output[number.length()];
+    }
+	public static void main(String[] args) {
+		// Write your code here
+		Scanner sc =  new Scanner(System.in);
+      	String input = sc.next();
+      	while(!input.equals("0"))
+        {
+      		System.out.println(give_no(input));
+          	input = sc.next();
+        }
+	}
+
+}
