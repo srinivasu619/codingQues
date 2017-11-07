1d1d7ee2625f2df19e9d023afba73e68b6a7e5ee
srinivasu619 <srinivasu.618@gmail.com>
2017-11-07 23:56:29 +0530

    Alpha Code Complete

--- Solution32.java
+++ Solution32.java
@@ -9,9 +9,12 @@ public class Solution32 {
       output[1] = 1;
       for(int i=2;i<=number.length();i++)
       {
-        output[i] = output[i-1]%1000000007;
+        if((number.charAt(i-1)-'0') > 0)
+          output[i] = output[i-1]%1000000007;
+        else
+          output[i] = 0;
         int num = (number.charAt(i-2)-'0')*10+(number.charAt(i-1)-'0');
-        if(num <= 26)
+        if(num>=10 && num<= 26)
         {
           output[i] = (output[i]%1000000007+output[i-2]%1000000007)%1000000007;
         }
